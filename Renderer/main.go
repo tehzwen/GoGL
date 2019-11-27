7b6b49ea733ebf07e151680b4e37b526a7c69db4
User <user@Depts-iMac.local>
2019-11-27 11:03:35 -0700

    Add goroutine for object math to be rendered

--- Renderer/main.go
+++ Renderer/main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"runtime"
+	"sync"
 
 	"./game"
 	"./geometry"
@@ -17,6 +18,22 @@ import (
 var keys map[glfw.Key]bool
 var buttons map[glfw.MouseButton]bool
 var mouseMovement map[string]float64
+var mu sync.Mutex
+var objectsToRender chan RenderObject
+
+type RenderObject struct {
+	viewMatrix      mgl32.Mat4
+	projMatrix      mgl32.Mat4
+	modelMatrix     mgl32.Mat4
+	currentBuffers  geometry.ObjectBuffers
+	currentModel    geometry.Model
+	currentMaterial geometry.Material
+	currentCentroid mgl32.Vec3
+	currentProgram  geometry.ProgramInfo
+	cameraPosition  []float32
+	currentVertices geometry.VertexValues
+	currentObject   geometry.Geometry
+}
 
 const (
 	width  = 1280
@@ -107,6 +124,8 @@ func main() {
 	//get arguments
 	argsWithoutProgram := os.Args[1:]
 
+	objectsToRender = make(chan RenderObject, 10)
+
 	keys = make(map[glfw.Key]bool)
 	buttons = make(map[glfw.MouseButton]bool)
 	mouseMovement = make(map[string]float64)
@@ -188,88 +207,115 @@ func draw(window *glfw.Window, state *geometry.State) {
 		lightStrengthArray = append(lightStrengthArray, state.Lights[t].Strength)
 	}
 
+	//getting the math values for each object using go routines
 	for i := 0; i < len(state.Objects); i++ {
+		go doObjectMath(state.Objects[i], (*state), objectsToRender)
+	}
 
-		currentProgramInfo, err := state.Objects[i].GetProgramInfo()
-
-		if err != nil {
-			//else lets throw an error here
-			fmt.Printf("ERROR getting program info!")
-		}
-		gl.UseProgram(currentProgramInfo.Program)
-		//now get the model
-		currentModel, err := state.Objects[i].GetModel()
-		if err != nil {
-			//throw an error
-			fmt.Printf("ERROR getting model!")
-		}
-
-		currentCentroid := state.Objects[i].GetCentroid()
-		currentMaterial := state.Objects[i].GetMaterial()
-		currentBuffers := state.Objects[i].GetBuffers()
-		currentVertices := state.Objects[i].GetVertices()
-
-		//fmt.Printf("%v+\n", currentBuffers)
-
-		var fovy = float32(60 * math.Pi / 180)
-		var aspect = float32(width / height)
-		var near = float32(0.1)
-		var far = float32(100.0)
-
-		//projection matrix
-		projection := mgl32.Perspective(fovy, aspect, near, far)
-		gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.Projection, 1, false, &projection[0])
-
-		//view matrix
-		viewMatrix := mgl32.LookAtV(state.Camera.Position, state.Camera.Center, state.Camera.Up)
-		gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.View, 1, false, &viewMatrix[0])
-		//update camera
-		camPosition := []float32{state.Camera.Position[0], state.Camera.Position[1], state.Camera.Position[2]}
-		gl.Uniform3fv(currentProgramInfo.UniformLocations.CameraPosition, 1, &camPosition[0])
-		state.ViewMatrix = viewMatrix
-
-		//model matrix
-
-		modelMatrix := mgl32.Ident4()
-		positionMat := mgl32.Translate3D(currentModel.Position[0], currentModel.Position[1], currentModel.Position[2])
-		modelMatrix = modelMatrix.Mul4(positionMat)
-		centroidMat := mgl32.Translate3D(currentCentroid[0], currentCentroid[1], currentCentroid[2])
-		modelMatrix = modelMatrix.Mul4(centroidMat)
-		modelMatrix = modelMatrix.Mul4(currentModel.Rotation)
-		negCent := mgl32.Translate3D(-currentCentroid[0], -currentCentroid[1], -currentCentroid[2])
-		modelMatrix = modelMatrix.Mul4(negCent)
-
-		modelMatrix = geometry.ScaleM4(modelMatrix, currentModel.Scale)
-
-		gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.Model, 1, false, &modelMatrix[0])
-
-		gl.Uniform3fv(currentProgramInfo.UniformLocations.DiffuseVal, 1, &currentMaterial.Diffuse[0])
-		gl.Uniform3fv(currentProgramInfo.UniformLocations.AmbientVal, 1, &currentMaterial.Ambient[0])
-		gl.Uniform3fv(currentProgramInfo.UniformLocations.SpecularVal, 1, &currentMaterial.Specular[0])
-		gl.Uniform1f(currentProgramInfo.UniformLocations.NVal, currentMaterial.N)
-
-		gl.Uniform1i(currentProgramInfo.UniformLocations.NumLights, int32(len(state.Lights)))
-
-		//update lights
-		if len(lightPositionArray) > 0 && len(lightColorArray) > 0 && len(lightStrengthArray) > 0 {
-			gl.Uniform3fv(currentProgramInfo.UniformLocations.LightPositions, int32(len(lightPositionArray)/3), &lightPositionArray[0])
-			gl.Uniform3fv(currentProgramInfo.UniformLocations.LightColours, int32(len(lightColorArray)/3), &lightColorArray[0])
-			gl.Uniform1fv(currentProgramInfo.UniformLocations.LightStrengths, int32(len(lightStrengthArray)), &lightStrengthArray[0])
-		}
-
-		gl.BindVertexArray(currentBuffers.Vao)
-		if state.Objects[i].GetType() != "mesh" {
-			gl.DrawElements(gl.TRIANGLES, int32(len(currentVertices.Vertices)), gl.UNSIGNED_INT, gl.Ptr(nil))
-		} else {
-			gl.DrawArrays(gl.TRIANGLES, 0, int32(len(currentVertices.Vertices)))
-		}
-
-		gl.BindVertexArray(0)
-
-		//window.SwapBuffers()
+	//render sequentially using channel values
+	for x := 0; x < len(state.Objects); x++ {
+		tempObject := <-objectsToRender
+		renderObject(state, tempObject, lightPositionArray, lightColorArray, lightStrengthArray)
 	}
 	window.SwapBuffers()
 
+}
+
+func renderObject(state *geometry.State, object RenderObject, lightPositionArray []float32, lightColorArray []float32, lightStrengthArray []float32) {
+	currentProgramInfo := object.currentProgram
+	projection := object.projMatrix
+	viewMatrix := object.viewMatrix
+	camPosition := object.cameraPosition
+	modelMatrix := object.modelMatrix
+	currentMaterial := object.currentMaterial
+	currentBuffers := object.currentBuffers
+	currentVertices := object.currentVertices
+
+	gl.UseProgram(currentProgramInfo.Program)
+
+	gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.Projection, 1, false, &projection[0])
+	gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.View, 1, false, &viewMatrix[0])
+	gl.Uniform3fv(currentProgramInfo.UniformLocations.CameraPosition, 1, &camPosition[0])
+	gl.UniformMatrix4fv(currentProgramInfo.UniformLocations.Model, 1, false, &modelMatrix[0])
+
+	gl.Uniform3fv(currentProgramInfo.UniformLocations.DiffuseVal, 1, &currentMaterial.Diffuse[0])
+	gl.Uniform3fv(currentProgramInfo.UniformLocations.AmbientVal, 1, &currentMaterial.Ambient[0])
+	gl.Uniform3fv(currentProgramInfo.UniformLocations.SpecularVal, 1, &currentMaterial.Specular[0])
+	gl.Uniform1f(currentProgramInfo.UniformLocations.NVal, currentMaterial.N)
+
+	gl.Uniform1i(currentProgramInfo.UniformLocations.NumLights, int32(len(state.Lights)))
+
+	//update lights
+	if len(lightPositionArray) > 0 && len(lightColorArray) > 0 && len(lightStrengthArray) > 0 {
+		gl.Uniform3fv(currentProgramInfo.UniformLocations.LightPositions, int32(len(lightPositionArray)/3), &lightPositionArray[0])
+		gl.Uniform3fv(currentProgramInfo.UniformLocations.LightColours, int32(len(lightColorArray)/3), &lightColorArray[0])
+		gl.Uniform1fv(currentProgramInfo.UniformLocations.LightStrengths, int32(len(lightStrengthArray)), &lightStrengthArray[0])
+	}
+
+	state.ViewMatrix = viewMatrix
+	gl.BindVertexArray(currentBuffers.Vao)
+	if object.currentObject.GetType() != "mesh" {
+		gl.DrawElements(gl.TRIANGLES, int32(len(currentVertices.Vertices)), gl.UNSIGNED_INT, gl.Ptr(nil))
+	} else {
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(currentVertices.Vertices)))
+	}
+
+	gl.BindVertexArray(0)
+}
+
+func doObjectMath(object geometry.Geometry, state geometry.State, objects chan<- RenderObject) {
+	currentProgramInfo, err := object.GetProgramInfo()
+
+	if err != nil {
+		//else lets throw an error here
+		fmt.Printf("ERROR getting program info!")
+	}
+
+	//now get the model
+	currentModel, err := object.GetModel()
+	if err != nil {
+		//throw an error
+		fmt.Printf("ERROR getting model!")
+	}
+
+	currentCentroid := object.GetCentroid()
+	currentMaterial := object.GetMaterial()
+	currentBuffers := object.GetBuffers()
+	currentVertices := object.GetVertices()
+
+	var fovy = float32(60 * math.Pi / 180)
+	var aspect = float32(width / height)
+	var near = float32(0.1)
+	var far = float32(100.0)
+
+	projection := mgl32.Perspective(fovy, aspect, near, far)
+	viewMatrix := mgl32.LookAtV(state.Camera.Position, state.Camera.Center, state.Camera.Up)
+	camPosition := []float32{state.Camera.Position[0], state.Camera.Position[1], state.Camera.Position[2]}
+	modelMatrix := mgl32.Ident4()
+	positionMat := mgl32.Translate3D(currentModel.Position[0], currentModel.Position[1], currentModel.Position[2])
+	modelMatrix = modelMatrix.Mul4(positionMat)
+	centroidMat := mgl32.Translate3D(currentCentroid[0], currentCentroid[1], currentCentroid[2])
+	modelMatrix = modelMatrix.Mul4(centroidMat)
+	modelMatrix = modelMatrix.Mul4(currentModel.Rotation)
+	negCent := mgl32.Translate3D(-currentCentroid[0], -currentCentroid[1], -currentCentroid[2])
+	modelMatrix = modelMatrix.Mul4(negCent)
+
+	modelMatrix = geometry.ScaleM4(modelMatrix, currentModel.Scale)
+
+	result := RenderObject{
+		modelMatrix:     modelMatrix,
+		viewMatrix:      viewMatrix,
+		projMatrix:      projection,
+		cameraPosition:  camPosition,
+		currentBuffers:  currentBuffers,
+		currentCentroid: currentCentroid,
+		currentMaterial: currentMaterial,
+		currentModel:    currentModel,
+		currentProgram:  currentProgramInfo,
+		currentVertices: currentVertices,
+		currentObject:   object,
+	}
+	objects <- result
 }
 
 // initGlfw initializes glfw and returns a Window to use.
